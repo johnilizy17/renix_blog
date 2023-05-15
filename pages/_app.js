391e0d687ce0844b8155c61ce954f98a1caefdec
johnilizy17 <johnabrahamtosin@gmail.com>
2023-05-15 23:07:24 +0100

    the updated version

--- pages/_app.js
+++ pages/_app.js
@@ -1,60 +1,14 @@
-import React from "react";
-import {
-  ChakraProvider,
-  ColorModeProvider,
-  useColorMode,
-} from "@chakra-ui/react";
-import customTheme from "../styles/theme";
-import { Global, css } from "@emotion/react";
-import { prismLightTheme, prismDarkTheme } from "../styles/prism";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
-const GlobalStyle = ({ children }) => {
-  const { colorMode } = useColorMode();
-
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
-      <Global
-        styles={css`
-          ${colorMode === `light` ? prismLightTheme : prismDarkTheme};
-          ::selection {
-            background-color: #90cdf4;
-            color: #fefefe;
-          }
-          ::-moz-selection {
-            background: #ffb7b7;
-            color: #fefefe;
-          }
-          html {
-            min-width: 356px;
-            scroll-behavior: smooth;
-          }
-          #__next {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-            background: ${colorMode === "light" ? "white" : "#171717"};
-          }
-        `}
-      />
-      {children}
-    </>
-  );
-};
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider resetCSS theme={customTheme}>
-      <ColorModeProvider
-        options={{
-          initialColorMode: "light",
-          useSystemColorMode: true,
-        }}
-      >
-        <GlobalStyle>
-          <Component {...pageProps} />
-        </GlobalStyle>
-      </ColorModeProvider>
-    </ChakraProvider>
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
   );
 }
 
